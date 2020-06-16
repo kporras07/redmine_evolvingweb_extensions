b63c16b5d9f2e29491c03fa0184d0c308699072e
Kevin Porras <kporras07@gmail.com>
2020-06-16 09:03:50 -0600

    refs #21486: Initial PoC of question-mark event.

--- assets/javascripts/inject_subtask_clone_link.js
+++ assets/javascripts/inject_subtask_clone_link.js
@@ -14,4 +14,26 @@ jQuery(function(){
       .attr('href', addLink.attr('href') + "&copy_from=" + issueId)
       .appendTo(addLink.parent());
   }
+
+  function addQuestionMark() {
+    $questionMarkModal = $('<div class="question-mark-modal"></div>');
+    $questionMarkModal.append('<iframe width="500px" height="500px" src="' + window.location.origin + '/redmine_shortcuts/help"></iframe>');
+    $('body').append($questionMarkModal);
+    $(document).on('keypress', function(e) {
+      e = e || window.event;
+      var charCode = e.keyCode || e.which;
+      console.log(charCode, 'CHAR CODE');
+      // 63 = '?' key
+      // '?' key toggles the modal
+      if (charCode === 63) {
+        $questionMarkModal.addClass('question-mark-modal-visible');
+      }
+      if (charCode === 27) {
+        $questionMarkModal.removeClass('question-mark-modal-visible');
+      }
+    });
+  }
+  addQuestionMark();
+
+  console.log('hello world!');
 });
